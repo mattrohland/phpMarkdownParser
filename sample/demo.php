c909054a77e8cf8f8a8faac031fe10a9913a38ac
Matthew Rohland <mattrohland@gmail.com>
2012-11-04 01:35:48 -0400

    Improved demo page styling

--- sample/demo.php
+++ sample/demo.php
@@ -14,63 +14,62 @@ $markdown = new Markdown();
 	h4,
 	h5,
 	h6,
-	p,
-	code,
-	.html{margin: 0 0 1em 0;}
+	.snippetCode{margin: 0 0 1em 0;}
 	body{
 		font-family: helvetica, sans-serif;
 		padding: 40px;
 	}
-	code{
+	.snippetCode{
 		display: block;
 		background: #EEEEEE;
 		border: 1px solid #E0E0E0;
 		white-space: pre;
 		padding: 20px;
 	}
-	.html{
+	.htmlPreview{
 		margin-top: 20px;
+		margin-bottom: 3em;
 		border: 1px dashed #DDDDDD;
 		padding: 20px;
 	}
 </style>
 </head>
 <body>
-<h1>Demo</h1>
+<h1>PHP Markdown Parser - Demo</h1>
 
 <h2>Headlines</h2>
-<code><?= $snippet = @file_get_contents('snippets/headlines.md'); ?></code>
-<div class="html">
+<code class="snippetCode"><?= $snippet = @file_get_contents('snippets/headlines.md'); ?></code>
+<div class="htmlPreview">
 <?= $markdown->get( $snippet ); ?>
 </div>
 
 <h2>Paragraphs and line breaks</h2>
-<code><?= $snippet = @file_get_contents('snippets/paragraphs.md'); ?></code>
-<div class="html">
+<code class="snippetCode"><?= $snippet = @file_get_contents('snippets/paragraphs.md'); ?></code>
+<div class="htmlPreview">
 <?= $markdown->get( $snippet ); ?>
 </div>
 
 <h2>Ordered list</h2>
-<code><?= $snippet = @file_get_contents('snippets/orderedList.md'); ?></code>
-<div class="html">
+<code class="snippetCode"><?= $snippet = @file_get_contents('snippets/orderedList.md'); ?></code>
+<div class="htmlPreview">
 <?= $markdown->get( $snippet ); ?>
 </div>
 
 <h2>Unordered list</h2>
-<code><?= $snippet = @file_get_contents('snippets/unorderedList.md'); ?></code>
-<div class="html">
+<code class="snippetCode"><?= $snippet = @file_get_contents('snippets/unorderedList.md'); ?></code>
+<div class="htmlPreview">
 <?= $markdown->get( $snippet ); ?>
 </div>
 
 <h2>Emphasis</h2>
-<code><?= $snippet = @file_get_contents('snippets/emphasis.md'); ?></code>
-<div class="html">
+<code class="snippetCode"><?= $snippet = @file_get_contents('snippets/emphasis.md'); ?></code>
+<div class="htmlPreview">
 <?= $markdown->get( $snippet ); ?>
 </div>
 
 <h2>Image</h2>
-<code><?= $snippet = @file_get_contents('snippets/image.md'); ?></code>
-<div class="html">
+<code class="snippetCode"><?= $snippet = @file_get_contents('snippets/image.md'); ?></code>
+<div class="htmlPreview">
 <?= $markdown->get( $snippet ); ?>
 </div>
 
